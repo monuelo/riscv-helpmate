7501cc63015f25f056d2c073d0d40859c4135e86
Héricles <hericles.me@gmail.com>
2018-12-18 19:37:12 -0300

    favicon & settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'RISC-V32I Helpmate',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    description: 'RISC-V Helpmate - Computação@UFCG - (LOAC) Laboratório de Organização e Arquitetura de Computadores.',
     author: '@hericlesme',
   },
   plugins: [
@@ -24,7 +24,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
